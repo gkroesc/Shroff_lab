e6830a6d20b0dd392b478992a8730b3c686fb59d
gkroesc <84924498+gkroesc@users.noreply.github.com>
2023-07-12 09:42:50 -0400

    Update Annotations_AT_to_mipav.py

--- Untwisting/Lineaging/Annotations_AT_to_mipav.py
+++ Untwisting/Lineaging/Annotations_AT_to_mipav.py
@@ -5,99 +5,109 @@ import numpy as np
 
 
 
-zip_path = "X:\\shrofflab\\efn-2\\121922_lin\\Pos2\\SPIMB\\Reg_Sample\\For_Lineaging\\StarryNite\\"
-cell_key_path = "C:\\Users\\kroeschellg\\Downloads\\Worm_untwisting_project\\Untwisting\\Lineaging\\CellNamer\\"
-
-timepoint = 50
-
-columns=['name','x_voxels','y_voxels','z_voxels','R','G','B']
-
-if not os.path.exists(zip_path + "For_mipav\\RegB\\Decon_reg_{}\\Decon_reg_{}_results\\integrated_annotation".format(timepoint,timepoint)):
-    os.makedirs(zip_path + "For_mipav\\RegB\\Decon_reg_{}\\Decon_reg_{}_results\\integrated_annotation".format(timepoint,timepoint))
+zip_path = "X:\\shrofflab\\efn-1\\lineaging\\For_Lineaging\\StarryNite\\"
 
 
-def FileGrabber(zip_path, timepoint):
+start_tp = 10
 
-    with zipfile.ZipFile(zip_path + 'SN_Files\\Decon_emb1_edited.zip', 'r') as zip_file:
+end_tp = 15
 
-        ztimepoint = str(timepoint).zfill(3)
+p1_end_tp = end_tp +1
 
-        file_path = 'nuclei/t{}-nuclei'.format(ztimepoint)
+def Acetree_to_MIPAV(zip_path, timepoint):
 
-        with zip_file.open(file_path, 'r') as text_file:
+    columns=['name','x_voxels','y_voxels','z_voxels','R','G','B']
 
-            lines = [line.decode('utf-8').strip() for line in text_file]
-
-        df = pd.DataFrame([line.split(', ') for line in lines])
-
-    return df
+    if not os.path.exists(zip_path + "For_mipav\\RegB\\Decon_reg_{}\\Decon_reg_{}_results\\integrated_annotation".format(timepoint,timepoint)):
+        os.makedirs(zip_path + "For_mipav\\RegB\\Decon_reg_{}\\Decon_reg_{}_results\\integrated_annotation".format(timepoint,timepoint))
 
 
+    def FileGrabber(zip_path, timepoint):
+
+        with zipfile.ZipFile(zip_path + 'SN_Files\\Decon_emb1_edited.zip', 'r') as zip_file:
+
+            ztimepoint = str(timepoint).zfill(3)
+
+            file_path = 'nuclei/t{}-nuclei'.format(ztimepoint)
+
+            with zip_file.open(file_path, 'r') as text_file:
+
+                lines = [line.decode('utf-8').strip() for line in text_file]
+
+            df = pd.DataFrame([line.split(', ') for line in lines])
+
+        return df
 
 
-def row_extractor(input_df):
-    new_df = pd.DataFrame(columns=columns)
 
-    for index, row in input_df.iterrows():
 
-        if row[9]:
+    def row_extractor(input_df):
+        new_df = pd.DataFrame(columns=columns)
 
-            column9_value = row[9]
-            column5_value = row[5]
-            column6_value = row[6]
-            column7_value = row[7]
+        for index, row in input_df.iterrows():
 
-            new_row = {'name':column9_value, 'x_voxels': column5_value, 'y_voxels':column6_value, 'z_voxels':column7_value, 'R':255, 'G':255, 'B':255}
-            new_row_df = pd.DataFrame(new_row, index=[0])
-            new_df = pd.concat([new_df, new_row_df],ignore_index=True)
-    return new_df
+            if row[9]:
 
-def cellNamer(input_df):
+                column9_value = row[9]
+                column5_value = row[5]
+                column6_value = row[6]
+                column7_value = row[7]
+
+                new_row = {'name':column9_value, 'x_voxels': column5_value, 'y_voxels':column6_value, 'z_voxels':column7_value, 'R':255, 'G':255, 'B':255}
+                new_row_df = pd.DataFrame(new_row, index=[0])
+                new_df = pd.concat([new_df, new_row_df],ignore_index=True)
+        return new_df
+
+    def cellNamer(input_df):
     
-    cellKeyMaster = cell_key_path + 'cellkeyMaster.csv'
-    seam_cells = ['H0L','H1L','H2L','V1L','V2L','V3L','V4L','V5L','V6L','TL','H0R','H1R','H2R','V1R','V2R','V3R','V4R','V5R','V6R','TR']
+        cellKeyMaster = 'C:\\Users\\kroeschellg\\Downloads\\Worm_untwisting_project_old\\Segmentation\\Lineaging\\CellNamer\\cellkeyMaster.csv'
+        seam_cells = ['H0L','H1L','H2L','V1L','V2L','V3L','V4L','V5L','V6L','TL','H0R','H1R','H2R','V1R','V2R','V3R','V4R','V5R','V6R','TR']
 
 
-    masterDF = pd.read_csv(cellKeyMaster)
-    masterDF.dropna(inplace = True)
-    masterKey = masterDF.to_numpy()
+        masterDF = pd.read_csv(cellKeyMaster)
+        masterDF.dropna(inplace = True)
+        masterKey = masterDF.to_numpy()
 
     
     
-    userAnnotDF = input_df
-    userKey = userAnnotDF.to_numpy()
-    updateduserKey = userAnnotDF.to_numpy()
-    threeNameList = []
-    for i, linName in enumerate(userKey[:, 0]):
-        for j, name in enumerate(masterKey[:,1]):
-            lowlinName = linName.lower().strip()
-            namerep = name.replace(" ", "").lower()
-            if lowlinName == namerep:
-                threeName = masterKey[j,0]
-                if threeName in seam_cells:
-                    updateduserKey[i,0] = threeName
-                else:
-                    updateduserKey[i,0] = threeName.lower()
-                #threeNameList.append(threeName)
+        userAnnotDF = input_df
+        userKey = userAnnotDF.to_numpy()
+        updateduserKey = userAnnotDF.to_numpy()
+        threeNameList = []
+        for i, linName in enumerate(userKey[:, 0]):
+            for j, name in enumerate(masterKey[:,1]):
+                lowlinName = linName.lower().strip()
+                namerep = name.replace(" ", "").lower()
+                if lowlinName == namerep:
+                    threeName = masterKey[j,0]
+                    if threeName in seam_cells:
+                        updateduserKey[i,0] = threeName
+                    else:
+                        updateduserKey[i,0] = threeName.lower()
+                    #threeNameList.append(threeName)
 
 
-    Updated_DF = pd.DataFrame(updateduserKey, columns=columns)
-
-
-
-
-    return Updated_DF
-
-
-AT_df = FileGrabber(zip_path, timepoint)
-
-
-new_df = row_extractor(AT_df)
+        Updated_DF = pd.DataFrame(updateduserKey, columns=columns)
 
 
 
-named_df = cellNamer(new_df)
+
+        return Updated_DF
+
+
+    AT_df = FileGrabber(zip_path, timepoint)
+
+
+    new_df = row_extractor(AT_df)
 
 
 
-named_df.to_csv(zip_path + "For_mipav\\RegB\\Decon_reg_{}\\Decon_reg_{}_results\\integrated_annotation\\annotations.csv".format(timepoint,timepoint), index=None)
+    named_df = cellNamer(new_df)
+
+
+
+    named_df.to_csv(zip_path + "For_mipav\\RegB\\Decon_reg_{}\\Decon_reg_{}_results\\integrated_annotation\\annotations.csv".format(timepoint,timepoint), index=None)
+
+
+for i in range(start_tp,p1_end_tp):
+    Acetree_to_MIPAV(zip_path,i)
